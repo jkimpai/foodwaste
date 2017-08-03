cf7a5c7c891bf6c2f9c7ace7bd531bdd77c581d1
Jacqueline Pai <jacquelinekpai@gmail.com>
2017-08-03 13:07:02 -0400

    changed color scheme

--- build.js
+++ build.js
@@ -62,11 +62,3 @@ function update() {
 
   });
 };
-
-function arcTween(a) {
-  var i = d3.interpolate(this._current, a);
-  this._current = i(0);
-  return function(t) {
-    return arc(i(t));
-  };
-};
